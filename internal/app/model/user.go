e3cfe754604fd7b7ed3fd4c01ae7a32aaf79480a
Altabaev <altabaev2013@gmail.com>
2021-09-22 13:27:01 +0300

    User registration

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID                int
-	Email             string
-	Password          string
-	EncryptedPassword string
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
 }
 
 func (u *User) Validate() error {
@@ -31,6 +31,10 @@ func (u *User) BeforeCreate() error {
 		u.EncryptedPassword = enc
 	}
 	return nil
+}
+
+func (u *User) Sanitize() {
+	u.Password = ""
 }
 
 func encryptString(password string) (string, error) {
